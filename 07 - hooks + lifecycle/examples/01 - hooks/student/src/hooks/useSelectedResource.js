4323ed509518364eff682bac4a9c973d48e380e7
SukhmanBajwa <sukhman.bajwa@outlook.com>
2026-03-15 18:53:11 -0600

    prog

---
+++ 07 - hooks + lifecycle/examples/01 - hooks/student/src/hooks/useSelectedResource.js
@@ -0,0 +1,9 @@
+import { useEffect, useState } from 'react';
+
+const STORAGE_KEY = 'selectedResource';
+
+export function useSelectedResource() {
+  const[selectedResource, setSelectedResource] = useState(() =>{
+    
+  })
+}
